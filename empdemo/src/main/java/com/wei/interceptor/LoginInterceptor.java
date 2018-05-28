0718d5aa7d587b8f7f9ebe86a4c3baeb686636ea
Weipeng <13546782483@163.com>
2018-05-28 16:25:44 +0800

    拦截器

---
+++ empdemo/src/main/java/com/wei/interceptor/LoginInterceptor.java
@@ -0,0 +1,59 @@
+package com.wei.interceptor;
+
+import com.wei.entity.User;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * 拦截器
+ */
+public class LoginInterceptor implements HandlerInterceptor {
+    /**
+     * 返回false controller就不执行了 controller之前执行
+     * @param httpServletRequest
+     * @param httpServletResponse
+     * @param o
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        HttpSession httpSession = httpServletRequest.getSession();
+        User user = (User) httpSession.getAttribute("user");
+        if(user==null){
+            httpServletResponse.sendRedirect("user/loginView");
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     *  controller之后执行  视图渲染之前执行
+     * @param httpServletRequest
+     * @param httpServletResponse
+     * @param o
+     * @param modelAndView
+     * @throws Exception
+     */
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    /**
+     * controller执行完 并且视图渲染之后执行
+     * @param httpServletRequest
+     * @param httpServletResponse
+     * @param o
+     * @param e
+     * @throws Exception
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
